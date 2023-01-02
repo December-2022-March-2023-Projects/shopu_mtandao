a293212f1d2d79270127d42d68472d28493dde5b
JudahSan <baraka.judah@yahoo.com>
2023-01-02 21:14:02 +0300

    product model

--- ecom_site/shopu/models.py
+++ ecom_site/shopu/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Merchandises(models.Model):
+  title = models.CharField(max_length=200)
+  price = models.FloatField()
+  discount_price = models.FloatField()
+  category = models.CharField(max_length=200)
+  # Textfield is more detailed
+  description = models.TextField()
+  image = models.CharField(max)
