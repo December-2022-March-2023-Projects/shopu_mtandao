7f1da2140ea3da38b8f881d8b81284b9485018a8
JudahSan <baraka.judah@yahoo.com>
2023-01-03 01:07:29 +0300

    search functionality

--- ecom_site/shopu/views.py
+++ ecom_site/shopu/views.py
@@ -4,4 +4,8 @@ from .models import Merchandises
 
 def index(request):
   merch_objects = Merchandises.objects.all()
+
+  item_name = request.GET.get('item_name')
+  if item_name != '' and item_name is not None:
+    merch_objects = merch_objects.filter(title__icontains=item_name)
   return render(request, 'shopu/index.html',{'merch_objects':merch_objects})
