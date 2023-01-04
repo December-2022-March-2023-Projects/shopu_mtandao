b1875132fd36002da5c35b19f2419d810944eab8
JudahSan <baraka.judah@yahoo.com>
2023-01-04 12:16:28 +0300

    django admin headers

--- ecom_site/shopu/admin.py
+++ ecom_site/shopu/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 from .models import Merchandises, Order
 # Register your models here.
 
+# Modify admin site
+admin.site.site_header = "Shop till you drop"
+admin.site.site_title = "Merch Shop"
+admin.site.index_title = "Manage Merch Shop Shopping"
+
 admin.site.register(Merchandises)
 admin.site.register(Order)
