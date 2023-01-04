c9b18820a44ce73beef4746663c1389df5f42ff1
JudahSan <baraka.judah@yahoo.com>
2023-01-04 12:32:08 +0300

    Added custom search field, modifying action list

--- ecom_site/shopu/admin.py
+++ ecom_site/shopu/admin.py
@@ -9,7 +9,13 @@ admin.site.index_title = "Manage Merch Shop Shopping"
 
 # list orders
 class MerchAdmin(admin.ModelAdmin):
+
+  def adjust_category_to_default(self, request, queryset):
+    queryset.update(category="default")
+  adjust_category_to_default.short_description = "Default Category"
   list_display = ('title', 'price', 'discount_price', 'category', 'description', 'image')
+  search_fields = ('category',)
+  actions = ('adjust_category_to_default',)
 
 admin.site.register(Merchandises, MerchAdmin)
 admin.site.register(Order)
